477148a2f6b0813dc662665d07bf5b16f3cdfa67
Dan Florio <dflo@danflorio.com>
2018-09-06 14:55:40 -0400

    edit menu sizing

--- src/parts/navigation/top-nav/TopNav.js
+++ src/parts/navigation/top-nav/TopNav.js
@@ -11,14 +11,34 @@ import Navigation from '../Navigation';
 
 
 class TopNav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollTop: window.scrollY,
+      mobileOpen: false
+    }
+  }
+
+  componentDidMount() {
+    window.onscroll = () => {
+      this.setState({
+        scrollTop: window.scrollY
+      });
+    }
+  }
+
   render() {
+
+    const shrinkNav = this.state.scrollTop > 80;
+    const topNavClass = "top-nav" + (shrinkNav ? " top-nav--small" : "");
 
     if (this.props.match.url === "/") {
       return null;
     }
 
     return (
-      <div className="top-nav">
+      <div className={topNavClass}>
         <NavLink to="/">
           <img className="top-nav__avatar" src={avatar} />
         </NavLink>
